bb441a8d88c1da71c34b6854512a04af96c7e6dd
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-29 15:34:39 +0000

    updated App.css to a scss file and copied in styling from rails app, added stying to Post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Timeline from './Timeline'
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
--- src/Post.js
+++ src/Post.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function Post(props) {
   return (
-    <div className="card">
-      <div>{props.post.userId}</div>
-      <div>{props.post.createdAt}</div>
-      <div>{props.post.message}</div>
+    <div className="card" style={{backgroundColor: '#20C997', marginBottom: '20px'}}>
+      <div className="card-header" style={{color: 'white'}}>
+        <div>{props.post.userId}</div>
+        <div>{props.post.createdAt}</div>
+      </div>
+      <div className="card-body" style={{backgroundColor: 'white'}}>
+        <div>{props.post.message}</div>
+      </div>
     </div>
   );
 }
