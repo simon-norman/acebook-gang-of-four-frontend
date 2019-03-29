3387988b0fa6a7e5691e6dd7350ae3118dc7ca4f
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-29 15:44:16 +0000

    move styling from Post to App.css file

--- src/Post.js
+++ src/Post.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Post(props) {
   return (
-    <div className="card" style={{backgroundColor: '#20C997', marginBottom: '20px'}}>
-      <div className="card-header" style={{color: 'white'}}>
+    <div className="card">
+      <div className="card-header">
         <div>{props.post.userId}</div>
         <div>{props.post.createdAt}</div>
       </div>
-      <div className="card-body" style={{backgroundColor: 'white'}}>
+      <div className="card-body">
         <div>{props.post.message}</div>
       </div>
     </div>
