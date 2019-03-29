3be4a2680ae9a71a6ecebcc02811893adaf0f3e9
Simon <01secn@gmail.com>
2019-03-29 12:18:53 +0000

    Displaying single post

---
+++ src/Post.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Post(props) {
+  return (
+    <div>{props.message}</div>
+  );
+}
+
+export default Post;
---
+++ src/Timeline.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Post from './Post'
+
+function Timeline() {
+  return (
+    <div>
+      <Post message='They took our JAAAAABS!' />
+    </div>
+  )
+}
+
+export default Timeline;
