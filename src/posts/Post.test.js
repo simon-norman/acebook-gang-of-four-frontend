c4575c40e1645177fa5ee8b29fda85d34408737d
Simon <01secn@gmail.com>
2019-04-04 10:11:36 +0100

    Fixed failing post test by adding user credentials to session storage in test set up

--- src/posts/Post.test.js
+++ src/posts/Post.test.js
@@ -4,6 +4,14 @@ import Post from './Post';
 import "../../test-setup.js";
 
 it('should render div for a post', () => {
+    const userCredentials = JSON.stringify({
+      'access-token': '12345',
+      client: 'chrome',
+      uid: '4'
+    })
+    sessionStorage.setItem('user', userCredentials)
+
+
     const mockPost = 
         {id: 1,
         message: "Hello", 
@@ -14,6 +22,5 @@ it('should render div for a post', () => {
       ;
 
     const wrapper = shallow(<Post post={mockPost} />);
-    expect(wrapper.html()).toEqual('<div class="card" id="1"><div class="card-header"><div>an email</div><div>2019-03-22</div></div><div class="card-body"><div>Hello</div></div></div>');
-
+    expect(wrapper.html()).toEqual('<div class="card" id="1"><div class="card-header"><div>an email</div><div>2019-03-22</div></div><div class="card-body"><div>Hello</div></div><div></div></div>');
 })
